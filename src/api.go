9fa01a18a025c8e0c5d6704aab08abcca0b21f03
Liam <liam@scalzulli.com>
2025-09-15 11:19:16 -0400

    Move `Document` type into `api` module

--- src/api.go
+++ src/api.go
@@ -9,16 +9,30 @@ import (
   "time"
 )
 
-type ReaderAPI struct {
-  token   string
-  baseURL string
-  client  *http.Client
+type Document struct {
+  ID            string `json:"id"`
+  Author        string `json:"author"`
+  Category      string `json:"category"`
+  HTMLContent   string `json:"html_content"`
+  Location      string `json:"location"`
+  PublishedDate any    `json:"published_date"`
+  SourceURL     string `json:"source_url"`
+  Summary       string `json:"summary"`
+  Title         string `json:"title"`
+  URL           string `json:"url"`
+  WordCount     int    `json:"word_count"`
 }
 
 type DocumentsResponse struct {
   Count          int        `json:"count"`
   NextPageCursor string     `json:"nextPageCursor"`
   Results        []Document `json:"results"`
+}
+
+type ReaderAPI struct {
+  token   string
+  baseURL string
+  client  *http.Client
 }
 
 func NewReaderAPI(token string) *ReaderAPI {
